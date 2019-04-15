be4c1b5ff97b33317857f912a4a2b1ae4e73c6b0
Sheehan Olver <solver@mac.com>
2019-04-15 19:55:48 +0100

    move tests, bump REQUIRE

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using ApproxFunSingularities, ApproxFunOrthogonalPolynomials, ApproxFunBase, InfiniteArrays, BlockArrays, IntervalSets, SpecialFunctions, LinearAlgebra, Random, Test
     import ApproxFunBase: HeavisideSpace, PointSpace, ArraySpace, DiracSpace, PiecewiseSegment, testbandedoperator, UnionDomain, testtransforms, testfunctional, testbandedblockbandedoperator
 
-
 @testset "Sqrt" begin
     x=Fun(identity);
     @time @test sqrt(cos(π/2*x))(.1) ≈ sqrt(cos(.1π/2))
@@ -28,6 +27,8 @@ using ApproxFunSingularities, ApproxFunOrthogonalPolynomials, ApproxFunBase, Inf
     f=Fun(x->exp(x)/sqrt(1-x.^2),JacobiWeight(-.5,-.5))
     @test f(.1) ≈ (x->exp(x)/sqrt(1-x.^2))(.1)
 
+    @time @test norm(Fun(exp,Legendre(0..1))+sqrt(Fun(0..1))) ≈ 2.491141949903508
+
     @testset "sampling Chebyshev" begin
         x=Fun(identity)
         f = exp(x)/sqrt(1-x^2)
@@ -50,6 +51,8 @@ using ApproxFunSingularities, ApproxFunOrthogonalPolynomials, ApproxFunBase, Inf
         ζ=Fun(identity,a)
         f=Fun(exp,a)*sqrt(abs((ζ-1)*(ζ-im)))
     end
+
+    @time @test norm(Fun(exp,Legendre(0..1))+sqrt(Fun(0..1))) ≈ 2.491141949903508
 end
 
 @testset "JacobiWeight" begin
@@ -623,11 +626,146 @@ include("IntegralEquationsTest.jl")
         @time v=F\f
         @test norm((u-v).coefficients)<100eps()
     end
+    @testset "Gaussian zero Dirichlet Poisson" begin
+        S=JacobiWeight(1.,1.,Jacobi(1.,1.))^2
+        Δ=Laplacian(S)
 
+        f=Fun((x,y)->exp(-10(x+.2)^2-20(y-.1)^2),rangespace(Δ))  #default is [-1,1]^2
+        @time v = \(Δ,f;tolerance=1E-14)
+        @test norm((Δ*v-f).coefficients) < 1E-14
+
+    end
     @testset "check we dispatch correctly to get fast build" begin
         S = JacobiWeight(1.,1.,Jacobi(1.,1.))^2
         Δ = Laplacian(S)
         @time S = view(Δ.op.ops[1].ops[1].op,Block.(1:40), Block.(1:40))
         @test typeof(S.parent.domaintensorizer) == ApproxFunBase.Trivial2DTensorizer
     end
+
+    @testset "Operator resize" begin
+        S=ChebyshevDirichlet()^2
+        B=Dirichlet(S)
+        f = Fun((x,y)->exp(x)*sin(y),S)
+        @test norm((Fun((x,y)->exp(x)*sin(y),∂(domain(S))) - B*f).coefficients) < 100eps()
+
+
+        S=JacobiWeight(1.,1.,Jacobi(1.,1.))^2
+        Δ=Laplacian(S)
+
+        @test cache(Δ)[1:100,1:100]  ≈ Δ[1:100,1:100]
+        @test cache(Δ;padding=true)[1:100,1:100]  ≈ Δ[1:100,1:100]
+
+        @test cache(Δ)[5:100,7:100]  ≈ Δ[5:100,7:100]
+        @test cache(Δ;padding=true)[5:100,7:100]  ≈ Δ[5:100,7:100]
+
+        # Check that QR is growing correctly
+        for col in (1,2,3,10,11,40)
+            QR=qr(Δ)
+            resizedata!(QR.R_cache,:,col+100)
+            resizedata!(QR,:,col)
+            QR2=qr!(CachedOperator(RaggedMatrix,Δ;padding=true))
+            resizedata!(QR2.R_cache,:,QR.ncols+100)
+            resizedata!(QR2,:,QR.ncols)
+            n=min(size(QR.H,1),size(QR2.H,1))
+            @test QR.H[1:n,1:col] ≈ QR2.H[1:n,1:col]
+            @test QR.R_cache[1:col,1:col] ≈ QR2.R_cache[1:col,1:col]
+            @test QR.R_cache[1:col+10,1:col+10] ≈ QR2.R_cache[1:col+10,1:col+10]
+        end
+
+        QRR=qr(Δ)
+        QR2=qr!(CachedOperator(RaggedMatrix,Δ;padding=true))
+        for col in (80,200)
+            resizedata!(QRR,:,col)
+            resizedata!(QR2,:,QRR.ncols)
+            n=min(size(QRR.H,1),size(QR2.H,1))
+            @test QRR.H[1:n,1:col] ≈ QR2.H[1:n,1:col]
+            @test QRR.R_cache[1:col,1:col] ≈ QR2.R_cache[1:col,1:col]
+            @test QRR.R_cache[1:col+10,1:col+10] ≈ QR2.R_cache[1:col+10,1:col+10]
+        end
+
+        # this checks a bug
+        QRR=qr(Δ)
+        resizedata!(QRR,:,548)
+        resizedata!(QRR,:,430)
+
+
+        u=Fun((x,y)->sin(π*x)*sin(π*y),S)
+        f=-2π^2*u
+
+
+        QRR=qr(Δ)
+        v=QRR\f
+        @test norm((u-v).coefficients)<100eps()
+
+        v=Δ\f
+        @test norm((u-v).coefficients)<100eps()
+
+
+        f=Fun((x,y)->exp(-10(x+.2)^2-20(y-.1)^2),rangespace(Δ))  #default is [-1,1]^2
+        @time v=\(Δ,f;tolerance=1E-14)
+        @test norm((Δ*v-f).coefficients)<1E-14
+
+        KO=Δ.op.ops[1].ops[1].op
+
+        M=BandedBlockBandedMatrix(view(KO,1:4,1:4))
+        @test norm(BandedBlockBandedMatrix(view(KO,1:4,2:4))-M[:,2:4]) < 10eps()
+        @test norm(BandedBlockBandedMatrix(view(KO,1:4,3:4))-M[:,3:4]) < 10eps()
+    end
+end
+
+
+@testset "Cauchy fun" begin
+    f = Fun((x,y)->1/(2π*(x^2+y^2+1)^(3/2)),Line()^2)
+    @test f(0.1,0.2) ≈ 1/(2π*(0.1^2+0.2^2+1)^(3/2))
+
+    #TODO: improve tolerance
+    f = LowRankFun((x,y)->1/(2π*(x^2+y^2+1)^(3/2)),JacobiWeight(2.,2.,Line())^2)
+    @test ≈(f(0.1,0.2),1/(2π*(0.1^2+0.2^2+1)^(3/2));atol=1E-4)
+end
+
+@testset "Bessel" begin
+    @time for ν in (1.,0.5,2.,3.5)
+        println("        ν = $ν")
+        S=JacobiWeight(-ν,0.,Chebyshev(0..1))
+        D=Derivative(S)
+        x=Fun(identity,domain(S))
+        L=(x^2)*D^2+x*D+(x^2-ν^2);
+        u=\([rdirichlet(S);rneumann(S);L],[bessely(ν,1.),.5*(bessely(ν-1.,1.)-bessely(ν+1.,1.)),0];
+                    tolerance=1E-10)
+        @test ≈(u(.1),bessely(ν,.1);atol=eps(1000000.)*max(abs(u(.1)),1))
+        u=Fun(x->bessely(ν,x),S)
+        @test ≈(u(.1),bessely(ν,.1);atol=eps(10000.)*max(abs(u(.1)),1))
+        u=Fun(x->besselj(ν,x),S)
+        @test ≈(u(.1),besselj(ν,.1);atol=eps(10000.)*max(abs(u(.1)),1))
+    end
+
+    @time for ν in (1.,0.5,0.123,3.5)
+        println("        ν = $ν")
+        S=JacobiWeight(ν,0.,Chebyshev(0..1))
+        D=Derivative(S)
+        x=Fun(identity,domain(S))
+        L=(x^2)*D^2+x*D+(x^2-ν^2);
+
+        u=\([rdirichlet(S);rneumann(S);L],[besselj(ν,1.),.5*(besselj(ν-1.,1.)-besselj(ν+1.,1.)),0];
+                    tolerance=1E-10)
+        @test ≈(u(.1),besselj(ν,.1);atol=eps(1000000.)*max(abs(u(.1)),1))
+        u=Fun(x->besselj(ν,x),S)
+        @test ≈(u(.1),besselj(ν,.1);atol=eps(10000.)*max(abs(u(.1)),1))
+    end
+end
+
+@testset "Speed test" begin
+    S = JacobiWeight(1.,1.,Jacobi(1.,1.))^2
+    Δ = Laplacian(S)
+
+    f = Fun((x,y)->sin(π*x)*sin(π*y),S)
+
+    QR1=qr(Δ)
+    ApproxFunBase.resizedata!(QR1,:,400)
+        \(QR1,f; tolerance=1E-10)
+    QR1=qr(Δ)
+        @time Δ[Block.(1:40), Block.(1:40)]
+        @time ApproxFunBase.resizedata!(QR1,:,400)
+        @time \(QR1,f; tolerance=1E-10)
+    println("Laplace Dirichlet: should be ~0.015, 0.015, 0.001")
 end
