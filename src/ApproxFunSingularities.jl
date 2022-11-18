ab21a6684857ed80f47c6ea276a1817735629935
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-11-18 23:06:28 +0400

    Clean up imports

--- src/ApproxFunSingularities.jl
+++ src/ApproxFunSingularities.jl
@@ -5,66 +5,35 @@ using Base, LinearAlgebra, Reexport, IntervalSets, DomainSets, Statistics
 @reexport using ApproxFunOrthogonalPolynomials
 
 
-import ApproxFunBase: normalize!, flipsign, FiniteRange, Fun, MatrixFun, UnsetSpace, VFun, RowVector,
-                    UnivariateSpace, AmbiguousSpace, SumSpace, SubSpace, WeightSpace, NoSpace, Space,
-                    HeavisideSpace, PointSpace,
-                    IntervalOrSegment, RaggedMatrix, AlmostBandedMatrix,
-                    AnyDomain, ZeroSpace, ArraySpace, TrivialInterlacer, BlockInterlacer,
-                    AbstractTransformPlan, TransformPlan, ITransformPlan,
-                    ConcreteConversion, ConcreteMultiplication, ConcreteDerivative, ConcreteIntegral,
-                    ConcreteVolterra, Volterra, VolterraWrapper,
-                    MultiplicationWrapper, ConversionWrapper, DerivativeWrapper, Evaluation, EvaluationWrapper,
-                    Conversion, defaultConversion, defaultcoefficients, default_Fun, Multiplication, Derivative, Integral, bandwidths,
-                    ConcreteEvaluation, ConcreteDefiniteLineIntegral, ConcreteDefiniteIntegral, ConcreteIntegral,
-                    DefiniteLineIntegral, DefiniteIntegral, ConcreteDefiniteIntegral, ConcreteDefiniteLineIntegral, IntegralWrapper,
-                    ReverseOrientation, ReverseOrientationWrapper, ReverseWrapper, Reverse, NegateEven, Dirichlet, ConcreteDirichlet,
-                    TridiagonalOperator, SubOperator, Space, @containsconstants, spacescompatible,
-                    hasfasttransform, canonicalspace, domain, setdomain, prectype, domainscompatible,
-                    plan_transform, plan_itransform, plan_transform!, plan_itransform!, transform, itransform, hasfasttransform,
-                    CanonicalTransformPlan, ICanonicalTransformPlan,
-                    Integral,
-                    domainspace, rangespace, boundary,
-                    union_rule, conversion_rule, maxspace_rule, conversion_type, maxspace, hasconversion, points,
-                    rdirichlet, ldirichlet, lneumann, rneumann, ivp, bvp,
-                    linesum, differentiate, integrate, linebilinearform, bilinearform,
-                    UnsetNumber, coefficienttimes, subspace_coefficients, sumspacecoefficients, specialfunctionnormalizationpoint,
-                    Segment, IntervalOrSegmentDomain, PiecewiseSegment, isambiguous, Vec, eps, isperiodic,
-                    arclength, complexlength,
-                    invfromcanonicalD, fromcanonical, tocanonical, fromcanonicalD, tocanonicalD, canonicaldomain, setcanonicaldomain, mappoint,
-                    reverseorientation, checkpoints, evaluate, extrapolate, mul_coefficients, coefficients, isconvertible,
-                    clenshaw, ClenshawPlan, sineshaw,
-                    toeplitz_getindex, toeplitz_axpy!, sym_toeplitz_axpy!, hankel_axpy!, ToeplitzOperator, SymToeplitzOperator, hankel_getindex,
-                    SpaceOperator, ZeroOperator, InterlaceOperator,
-                    interlace!, reverseeven!, negateeven!, cfstype, pad!, alternatesign!, mobius,
-                    extremal_args, hesseneigvals, chebyshev_clenshaw, recA, recB, recC, roots,splitatroots,
-                    chebmult_getindex, intpow, alternatingsum,
-                    domaintype, diagindshift, rangetype, weight, isapproxinteger, default_Dirichlet, scal!, dotu,
-                    components, promoterangespace, promotedomainspace,
-                    block, blockstart, blockstop, blocklengths, isblockbanded, pointscompatible, affine_setdiff, complexroots,
-                    ∞, gamma
+import ApproxFunBase: Fun, SumSpace, SubSpace, WeightSpace, NoSpace,
+            IntervalOrSegment, ConcreteMultiplication, ConcreteDerivative,
+            MultiplicationWrapper, ConversionWrapper, DerivativeWrapper,
+            Evaluation, Conversion, defaultConversion, defaultcoefficients,
+            Multiplication, Derivative, bandwidths,
+            ConcreteEvaluation, ConcreteDefiniteLineIntegral,
+            ConcreteDefiniteIntegral, ConcreteIntegral,
+            DefiniteLineIntegral, DefiniteIntegral, ConcreteDefiniteIntegral,
+            ConcreteDefiniteLineIntegral, maxspace, hasconversion,
+            Space, spacescompatible, canonicalspace, domain, setdomain,
+            domainscompatible, domainspace, rangespace, boundary,
+            union_rule, conversion_rule, maxspace_rule, conversion_type,
+            linesum, differentiate, integrate, linebilinearform, bilinearform,
+            subspace_coefficients, sumspacecoefficients,
+            specialfunctionnormalizationpoint, Segment, IntervalOrSegmentDomain,
+            Vec, eps, arclength, complexlength, tocanonical, fromcanonicalD,
+            tocanonicalD, canonicaldomain, setcanonicaldomain,
+            coefficients, isconvertible, SpaceOperator, cfstype, mobius, roots,
+            splitatroots, domaintype, rangetype, weight, isapproxinteger,
+            dotu, components, promoterangespace, ∞, gamma
 
 import ApproxFunOrthogonalPolynomials: order
 
-import DomainSets: Domain, indomain, UnionDomain, ProductDomain, FullSpace, Point, elements, DifferenceDomain,
-            Interval, ChebyshevInterval, boundary, ∂, rightendpoint, leftendpoint,
-            dimension
+import DomainSets: Domain, Interval, ChebyshevInterval, boundary,
+            rightendpoint, leftendpoint, dimension
 
-import Base: values, convert, getindex, setindex!, *, +, -, ==, <, <=, >, |, !, !=, eltype, iterate,
-                >=, /, ^, \, ∪, transpose, size, tail, broadcast, broadcast!, copyto!, copy, to_index, (:),
-                similar, map, vcat, hcat, hvcat, show, summary, stride, sum, cumsum, sign, imag, conj, inv,
-                complex, reverse, exp, sqrt, abs, abs2, sign, issubset, values, in, first, last, rand, intersect, setdiff,
-                isless, union, angle, join, isnan, isapprox, isempty, sort, merge, promote_rule,
-                minimum, maximum, extrema, argmax, argmin, findmax, findmin, isfinite,
-                zeros, zero, one, promote_rule, repeat, length, resize!, isinf,
-                getproperty, findfirst, unsafe_getindex, fld, cld, div, real, imag,
-                @_inline_meta, eachindex, firstindex, lastindex, keys, isreal, OneTo,
-                Array, Vector, Matrix, view, ones, @propagate_inbounds, print_array,
-                split, exp, log
-
-import LinearAlgebra: BlasInt, BlasFloat, norm, ldiv!, mul!, det, eigvals, dot, cross,
-                qr, qr!, rank, isdiag, istril, istriu, issymmetric, ishermitian,
-                Tridiagonal, diagm, diagm_container, factorize, nullspace,
-                Hermitian, Symmetric, adjoint, transpose, char_uplo
+import Base: convert, getindex, *, /, ^,
+            show, sum, cumsum, complex, sqrt, abs, in, first, last,
+            union, isapprox, zeros, one, length, ones, exp, log
 
 include("divide_singularity.jl")
 include("JacobiWeight.jl")
