1e6fc10e3a424e69995b0f12d647e72a9f84d8fd
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-09-13 13:42:14 +0400

    Fix ExpWeight constructor (#92)

--- test/runtests.jl
+++ test/runtests.jl
@@ -359,6 +359,14 @@ end
     end
 end
 
+@testset "ExpWeight" begin
+    S = ExpWeight(-Fun()^2, Chebyshev())
+    x = 0.5
+    n = 2
+    @test S(n, x) ≈ exp(-x^2) * Chebyshev()(n, x)
+    @test S(n)'(x) ≈ exp(-x^2) * Chebyshev()(n)'(x) - 2x * exp(-x^2) * Chebyshev()(n, x)
+end
+
 @testset "DiracDelta and PointSpace" begin
     a,b=DiracDelta(0.),DiracDelta(1.)
     f=Fun(exp)
